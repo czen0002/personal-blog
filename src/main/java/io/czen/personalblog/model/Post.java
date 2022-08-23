6197774e120fc5a281704d2b3ab17e6e80dda438
czen0002 <zc114789@gmail.com>
2022-08-23 23:54:39 +1000

    [CHENG] Add validation in Post

--- src/main/java/io/czen/personalblog/model/Post.java
+++ src/main/java/io/czen/personalblog/model/Post.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
@@ -18,8 +19,10 @@ public class Post {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotBlank(message = "{post.title.NotBlank}")
     private String title;
 
+    @NotBlank(message = "{post.content.NotNull}")
     private String content;
 
     private LocalDate dateCreated;
