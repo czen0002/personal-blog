1f941c07a291d04417199dc03793f3caeefbc769
czen0002 <zc114789@gmail.com>
2022-08-24 23:59:24 +1000

    [CHENG] Add list mapper

--- src/main/java/io/czen/personalblog/mapper/PostMapper.java
+++ src/main/java/io/czen/personalblog/mapper/PostMapper.java
@@ -4,10 +4,16 @@ import io.czen.personalblog.entity.PostEntity;
 import io.czen.personalblog.model.Post;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
     Post toPost(PostEntity postEntity);
 
     PostEntity toPostEntity(Post post);
+
+    List<Post> toPosts(List<PostEntity> postEntities);
+
+    List<PostEntity> toPostEntities(List<Post> posts);
 }
