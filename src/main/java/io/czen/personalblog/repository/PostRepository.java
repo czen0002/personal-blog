de1987d49cfb281f6e93e08842c3bdb7725e7267
czen0002 <zc114789@gmail.com>
2022-08-28 22:41:02 +1000

    [CHENG] Add a method

--- src/main/java/io/czen/personalblog/repository/PostRepository.java
+++ src/main/java/io/czen/personalblog/repository/PostRepository.java
@@ -4,6 +4,10 @@ import io.czen.personalblog.entity.PostEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends PagingAndSortingRepository<PostEntity, Long> {
+
+    Optional<PostEntity> findByTitle(String title);
 }
