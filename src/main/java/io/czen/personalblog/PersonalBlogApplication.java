0e766bf8847a23e67982ce79a8cc341c42fadca7
czen0002 <zc114789@gmail.com>
2022-08-29 23:39:58 +1000

    [CHENG] Enable jpa audit

--- src/main/java/io/czen/personalblog/PersonalBlogApplication.java
+++ src/main/java/io/czen/personalblog/PersonalBlogApplication.java
@@ -2,8 +2,10 @@ package io.czen.personalblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PersonalBlogApplication {
 
 	public static void main(String[] args) {
